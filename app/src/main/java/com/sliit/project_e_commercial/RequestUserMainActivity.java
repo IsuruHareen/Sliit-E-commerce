d872933b54090f4dd12ba0af8f554829427cb32c
Rashika1998 <it18213140@my.sliit.lk>
2020-03-19 14:01:41 +0530

    E-commercial application creation

---
+++ app/src/main/java/com/sliit/project_e_commercial/RequestUserMainActivity.java
@@ -0,0 +1,14 @@
+package com.sliit.project_e_commercial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequestUserMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_user_main);
+    }
+}
